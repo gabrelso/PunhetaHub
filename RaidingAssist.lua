873f192d20e03f9deaeb3b36d6e9107c6a41cfdc
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-23 15:37:57 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -5,11 +5,15 @@ local vim = game:GetService("VirtualInputManager")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
+local function waitForCharacter()
+    repeat wait() until workspace:WaitForChild("PlayerCharacters"):WaitForChild(LocalPlayer.Name)
+end
+
 local raidSelect = { [1] = RaidMode }
 game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(raidSelect))
 
 while getgenv().Enabled do
-    task.wait(0.65)
+    task.wait(1)
     game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(raidSelect)) 
     game:GetService("ReplicatedStorage").GoldenArenaEvents.StartEvent:FireServer()
     
@@ -19,10 +23,23 @@ while getgenv().Enabled do
 end
 
 LocalPlayer.CharacterAdded:Connect(function(character)
-    local characterModel = workspace:WaitForChild("PlayerCharacters"):WaitForChild(LocalPlayer.Name)
+    waitForCharacter()
+    
     if getgenv().AutoShiftlock then
         task.wait(2)
         vim:SendKeyEvent(true, "LeftShift", false, nil)
         vim:SendKeyEvent(false, "LeftShift", false, nil)
+    end
+end)
+
+game.Players.PlayerAdded:Connect(function(player)
+    if player == LocalPlayer then
+        waitForCharacter()
+        
+        if getgenv().AutoShiftlock then
+            task.wait(2)
+            vim:SendKeyEvent(true, "LeftShift", false, nil)
+            vim:SendKeyEvent(false, "LeftShift", false, nil)
+        end
     end
 end)
