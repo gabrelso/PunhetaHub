23801930cd13282dbc95cf498b0e7cd8e6c258f2
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-23 19:49:34 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -1,22 +1,9 @@
 -- SUPPOSED TO ASSIST PEOPLE WITH RAIDS 
 -- Made by lv7gab, don't sue my ass.
 
+local lp = game.Players.LocalPlayer
 local vim = game:GetService("VirtualInputManager")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-local args = { [1] = "Hard" }
-game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
-
-while getgenv().Enabled do
-    task.wait(1)
-    game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
-    game:GetService("ReplicatedStorage").GoldenArenaEvents.StartEvent:FireServer()
-    if getgenv().AutoSkip then
-        game:GetService("ReplicatedStorage").GoldenArenaEvents.SkipFunc:InvokeServer()
-    end
-end
-
+local _wait = task.wait
 local shiftLockApplied = false
 
 local function pressShift()
@@ -24,18 +11,27 @@ local function pressShift()
     vim:SendKeyEvent(false, Enum.KeyCode.LeftShift, false, game)
 end
 
-local function setupShiftLock()
-    if not shiftLockApplied then
-        task.wait(5)
-        if getgenv().AutoShiftlock then
-            pressShift()
-            shiftLockApplied = true
-        end
+local function waitForCharacter()
+    repeat _wait() until lp.Character and lp.Character:FindFirstChild("Humanoid")
+end
+
+local function applyShiftLock()
+    if not shiftLockApplied and getgenv().AutoShiftlock then
+        pressShift()
+        shiftLockApplied = true
     end
 end
 
-if LocalPlayer.Character then
-    setupShiftLock()
+local function handleCharacterSpawn()
+    shiftLockApplied = false
+    waitForCharacter()
+    applyShiftLock()
 end
 
-LocalPlayer.CharacterAdded:Connect(setupShiftLock)
+if lp.Character then
+    handleCharacterSpawn()
+end
+
+lp.CharacterAdded:Connect(function()
+    handleCharacterSpawn()
+end)
