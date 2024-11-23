779d53331ea73b7569e11f436480f08a9f5beb12
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-23 18:33:31 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -10,26 +10,24 @@ game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
 
 while getgenv().Enabled do
     task.wait(1)
-    game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args)) 
+    game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
     game:GetService("ReplicatedStorage").GoldenArenaEvents.StartEvent:FireServer()
-    
     if getgenv().AutoSkip then
         game:GetService("ReplicatedStorage").GoldenArenaEvents.SkipFunc:InvokeServer()
     end
 end
 
-local function pressShift()
-    vim:SendKeyEvent(true, "LeftShift", false, game)
-    vim:SendKeyEvent(false, "LeftShift", false, game)
+local function setupShiftLock()
+    task.wait(5)
+    if getgenv().AutoShiftlock then
+            vim:SendKeyEvent(true, "LeftShift", false, game)
+            vim:SendKeyEvent(false, "LeftShift", false, game)
+        end
+    end 
 end 
 
-game.Players.PlayerAdded:Connect(function(player) 
-    if player == LocalPlayer then
-        LocalPlayer.CharacterAdded:Connect(function(character)
-            task.wait(5)
-            if getgenv().AutoShiftlock then
-                pressShift()
-            end
-        end)
-    end
-end)
+if LocalPlayer.Character then
+    setupShiftLock()
+end
+
+LocalPlayer.CharacterAdded:Connect(setupShiftLock)
