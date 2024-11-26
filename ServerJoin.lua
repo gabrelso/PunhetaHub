2f8ca640fecf5cbb1dc7afbc9fe872f6ffd6da64
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-26 19:57:12 -0300

    Create ServerJoin.lua

---
+++ ServerJoin.lua
@@ -0,0 +1,21 @@
+local PlaceID
+
+if Sea == "1st" then
+    PlaceID = 4520749081
+elseif Sea == "2nd" then
+    PlaceID = 6381829480
+elseif Sea == "3rd" then
+    PlaceID = 15759515082
+end
+
+local function joinServer()
+    while true do
+        task.wait(1)
+        if game.PlaceId == PlaceID and game.PlaceId ~= 5931540094 and game.JobId ~= JobID then
+            Roblox.GameLauncher.joinGameInstance(PlaceID, JobID)
+        end
+    end
+end
+
+repeat task.wait() until game:IsLoaded()
+joinServer()
