f34b23a6ff4f19a7a055470ae423b85b2a80a0bf
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-26 20:08:15 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -5,7 +5,6 @@ end
 local lp = game.Players.LocalPlayer
 local vim = game:GetService("VirtualInputManager")
 local _wait = task.wait
-local shiftLockApplied = false
 
 local args = { [1] = "Hard" }
 
@@ -22,32 +21,7 @@ while getgenv().Enabled do
 end
 
 local function pressShift()
-    vim:SendKeyEvent(true, Enum.KeyCode.LeftShift, false, game)
-    _wait(0.1)
-    vim:SendKeyEvent(false, Enum.KeyCode.LeftShift, false, game)
+    vim:SendKeyEvent(true, "LeftShift", false, game)
+    vim:SendKeyEvent(false, "LeftShit", false, game)
 end
 
-local function waitForCharacter()
-    repeat _wait() until lp.Character and lp.Character:FindFirstChild("Humanoid")
-end
-
-local function applyShiftLock()
-    if not shiftLockApplied and getgenv().AutoShiftlock then
-        pressShift()
-        shiftLockApplied = true
-    end
-end
-
-local function handleCharacterSpawn()
-    shiftLockApplied = false
-    waitForCharacter()
-    applyShiftLock()
-end
-
-if lp.Character then
-    handleCharacterSpawn()
-end
-
-lp.CharacterAdded:Connect(function()
-    handleCharacterSpawn()
-end)
