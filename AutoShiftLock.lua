f3f64803a98f63992f7415ddc66ffb37c166113c
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-27 14:54:48 -0300

    Delete AutoShiftLock.lua

--- AutoShiftLock.lua
+++
@@ -1,8 +0,0 @@
-while true do
-    if workspace.MOB and workspace.MOB:FindFirstChild("Shadowthorn Cruelty") then
-        game:GetService("VirtualInputManager"):SendKeyEvent(true, "LeftShift", false, game)
-        game:GetService("VirtualInputManager"):SendKeyEvent(false, "LeftShift", false, game)
-        break
-    end
-    task.wait(1)
-end
