3ec27f7d0a9b4e2dece90ea58cc985816ec4afc2
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-26 21:17:04 -0300

    Update ServerJoin.lua

--- ServerJoin.lua
+++ ServerJoin.lua
@@ -1,12 +1,12 @@
 local TeleportService = game:GetService("TeleportService")
 
-if game.PlaceId ~= 4520749081 or game.PlaceId ~= 6381829480 or game.PlaceId ~= 15759515082 then
+if game.PlaceId ~= 4520749081 then
     return
 end
 
 local function JoinServer()
     while getgenv().Enabled do task.wait(1)
-        if game.JobId ~= JobID then
+        if game.JobId ~= JobID and if game.PlaceId == 4520749081 then
             print("Attempting to teleport to JobID:", JobID)
             TeleportService:TeleportToPlaceInstance(PlaceID, JobID)
         else
