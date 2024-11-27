88e9746007da77aa80df7bb6f2194cf119489c10
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-27 15:03:26 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -5,8 +5,11 @@ end
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local args = { [1] = RaidMode }
-game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
+local argsRaid = {
+    [1] = RaidMode
+}
+
+game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(argsRaid))
 
 task.spawn(function()
     while getgenv().Enabled do
