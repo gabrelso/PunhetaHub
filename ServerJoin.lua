d2bfb3bf1294844c189b1834ba61db9bcf38f957
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-26 22:28:27 -0300

    Update ServerJoin.lua

--- ServerJoin.lua
+++ ServerJoin.lua
@@ -1,6 +1,22 @@
 local TeleportService = game:GetService("TeleportService")
 
-if game.PlaceId ~= 4520749081 then
+local Sea = getgenv().Sea
+local JobID = getgenv().JobID
+
+local PlaceID
+
+if Sea == "1st" then
+    PlaceID = 4520749081
+elseif Sea == "2nd" then
+    PlaceID = 6381829480
+elseif Sea == "3rd" then
+    PlaceID = 15759515082
+else
+    print("Invalid Sea selection.")
+    return
+end
+
+if game.PlaceId ~= PlaceID then
     return
 end
 
@@ -8,9 +24,9 @@ local function JoinServer()
     while getgenv().Enabled do
         task.wait(1)
         
-        if game.JobId ~= getgenv().JobID then
-            print("Attempting to teleport to JobID:", getgenv().JobID)
-            TeleportService:TeleportToPlaceInstance(4520749081, getgenv().JobID)
+        if game.JobId ~= JobID then
+            print("Attempting to teleport to JobID:", JobID)
+            TeleportService:TeleportToPlaceInstance(PlaceID, JobID)
         else
             print("Already in the correct server.")
         end
