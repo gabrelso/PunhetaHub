85f2b90e2b2c08d5897a3306c89779ff2a5bc44c
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-27 14:57:48 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -2,14 +2,18 @@ if game.PlaceId ~= 5931540094 then
     return
 end
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
 local args = { [1] = RaidMode }
 game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
 
-while getgenv().Enabled do
-    task.wait(1)
-    game:GetService("ReplicatedStorage").GoldenArenaEvents.StartEvent:FireServer()
-    game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
-    if getgenv().AutoSkip then
-        game:GetService("ReplicatedStorage").GoldenArenaEvents.SkipFunc:InvokeServer()
+task.spawn(function()
+    while getgenv().Enabled do
+        task.wait(1)
+        game:GetService("ReplicatedStorage").GoldenArenaEvents.StartEvent:FireServer()
+        if getgenv().AutoSkip then
+            game:GetService("ReplicatedStorage").GoldenArenaEvents.SkipFunc:InvokeServer()
+        end
     end
-end
+end)
