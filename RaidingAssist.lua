7d9728a3070f8bb8207b8c28093e9b6163358b58
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-11-26 23:37:42 -0300

    Update RaidingAssist.lua

--- RaidingAssist.lua
+++ RaidingAssist.lua
@@ -2,28 +2,28 @@ if game.PlaceId ~= 5931540094 then
     return
 end
 
-local lp = game.Players.LocalPlayer
 local vim = game:GetService("VirtualInputManager")
-local _wait = task.wait
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
 local args = { [1] = "Hard" }
-
 game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
 
 while getgenv().Enabled do
-    _wait(1)
+    task.wait(1)
     game:GetService("ReplicatedStorage").ChooseMapRemote:FireServer(unpack(args))
     game:GetService("ReplicatedStorage").GoldenArenaEvents.StartEvent:FireServer()
-
     if getgenv().AutoSkip then
         game:GetService("ReplicatedStorage").GoldenArenaEvents.SkipFunc:InvokeServer()
     end
 end
 
-repeat wait() until game:IsLoaded()
-
-local function pressShift()
-    vim:SendKeyEvent(true, "LeftShift", false, game)
-    vim:SendKeyEvent(false, "LeftShit", false, game)
+local function setupShiftLock()
+    if getgenv().AutoShiftlock then
+        vim:SendKeyEvent(true, "LeftShift", false, game)
+        vim:SendKeyEvent(false, "LeftShift", false, game)
+    end
 end
 
+repeat wait() until workspace:FindFirstChild("MOB") and workspace.MOB:FindFirstChild("Shadowtorn Cruelty")
+setupShiftLock()
